0bf3d0b0e58698d50dec5f1b96fa42a47b74e8d2
Michael Cane <michaelcane@Zipcoder06s-MBP.fios-router.home>
2016-04-27 22:12:44 -0400

    committing a working calculator

---
+++ src/main/java/io/michaelcane/App.java
@@ -0,0 +1,11 @@
+package io.michaelcane;
+
+/**
+ * Created by michaelcane on 4/27/16.
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
